fac9faf87a524967ec535f41b4de0efd544529e2
mini <191342998+mini101591@users.noreply.github.com>
2025-05-21 21:54:14 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+class Header {
+  constructor() {
+    this.color = "Red";
+  }
+
+//Regular function:
+ // changeColor = function() {
+ //arrow function
+ changeColor = () => {
+    document.getElementById("demo").innerHTML += this;
+  }
+}
+
+const myheader = new Header();
+
+//The window object calls the function:
+window.addEventListener("load", myheader.changeColor);
+
+//A button object calls the function:
+//document.getElementById("btn").addEventListener("click", myheader.changeColor);
+
+
+// Array Methods
+const myArray = ['apple', 'banana', 'orange'];
+
+const myList = myArray.map((item) => <p>{item}</p>)
+
+//Destructing
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Arrow Function</h1>
+      <p>The <strong>this</strong> keyword represents the Header object.</p>
+      <button id="btn">Click Me!</button>
+      <p><strong>this</strong> represents:</p>
+      <p id="demo"></p>
+      <div>{myList}</div>
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const myFirstElement = <h1>Hello React!</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //root.render(myFirstElement);
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
